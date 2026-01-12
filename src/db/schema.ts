036a5a427975cc693d6ddc7c49900f6d33734a64
bishalr0y <bishalroy895@gmail.com>
2026-01-12 21:46:54 +0530

    add users table migratin

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,16 @@
 import * as p from "drizzle-orm/pg-core";
 
+export const usersTable = p.pgTable("users", {
+  id: p.uuid().defaultRandom().primaryKey(),
+  email: p.text().unique().notNull(),
+  password: p.text().notNull(),
+});
+
 export const urlsTable = p.pgTable("urls", {
   id: p.uuid().defaultRandom().primaryKey(),
   code: p.text().unique().notNull(),
   url: p.text().notNull().unique(),
   created_at: p.timestamp().defaultNow(),
+  // TODO: will add the line below when the user functionalities are done
+  // user_id_fk: p.uuid().references(() => usersTable.id, { onDelete: "cascade" }),
 });
