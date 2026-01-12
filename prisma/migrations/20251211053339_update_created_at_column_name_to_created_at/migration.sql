47dc510bf6be09c4dfaebb955a5bf60ba0eef428
bishalr0y <bishalroy895@gmail.com>
2026-01-12 21:31:52 +0530

    removed prisma

--- prisma/migrations/20251211053339_update_created_at_column_name_to_created_at/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `urls` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "urls" DROP COLUMN "createdAt",
-ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
